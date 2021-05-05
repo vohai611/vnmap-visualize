05c9d9b873c84df3a289c9ba836cee9fecee7c4a
vohai611 <hai835559@gmail.com>
2021-05-05 14:14:57 +0200

    reproduce company-labor report

--- Render-all-report.R
+++ Render-all-report.R
@@ -1,5 +1,13 @@
-###render final result
-rmarkdown::render("Analyzing-vn-companu-labor.Rmd",output_file = "final-result/Analyzing-company-labor")
+# render company's revenue and labor force report --------------------------------
+rmarkdown::render("Analyzing-vn-companu-labor.Rmd",
+                  output_file = "final-result/Analyzing-company-labor",envir = new.env())
 markdown::rpubsUpload(title = "first temp upload",
                       htmlFile = "final-result/Analyzing-company-labor.html")
-#
+
+
+
+# render education report -------------------------------------------------
+
+rmarkdown::render("Vietnam-education-report/Vietnam-education-report.Rmd",
+                  output_file = here("final-result/education-report"), envir = new.env())
+
